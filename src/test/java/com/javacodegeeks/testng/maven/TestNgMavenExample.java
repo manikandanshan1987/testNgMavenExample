434963dacbae7c121523f4d51f4b104efccd659d
manikandanshan1987 <53928324+manikandanshan1987@users.noreply.github.com>
2019-10-21 11:04:05 +0530

    commited

--- src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
@@ -6,6 +6,6 @@ public class TestNgMavenExample {
 
 	@Test
 	public void exampleOfTestNgMaven() {
-		System.out.println("This is TestNG-Maven Example");
+		.out.println("This is TestNG-Maven Example");
 	}
 }
